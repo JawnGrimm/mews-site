1404c58aad2006759564297b07cb0c2e28692796
JawnGrimm <jawngrimm@gmail.com>
2025-04-06 19:15:12 +0000

    Commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('newsFeed', () => ({
         // Replace 'YOUR_API_KEY' with your actual NewsAPI key
-        apiKey: 'YOUR_API_KEY',
+        apiKey: 38e9949744fa422383b4944c15a3f9b9,
         articles: [],
         async fetchNews() {
             const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${this.apiKey}`);
